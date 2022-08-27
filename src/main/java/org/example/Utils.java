daabc23b2e3af9d9d369fc373bd3ae2d4a999436
rukshan_d <rukshand20012@gmail.com>
2022-08-27 17:46:39 +0530

    recreate comments in utils class

--- src/main/java/org/example/Utils.java
+++ src/main/java/org/example/Utils.java
@@ -17,46 +17,65 @@ public class Utils {
         /*To withdraw money*/
 
         if (account.getAccountBalance() >= withdrawAmount){
+            /*For check account has sufficient balance*/
+
             double newBalance = account.getAccountBalance()-withdrawAmount;
             account.setAccountBalance(newBalance);
+
             System.out.println("Your withdraw successful and new balance is: "+newBalance);
+
         }else {
             System.out.println("Not sufficient balance");
         }
     }
 
     public void deposit(Account account,double depositAmount){
+        /*to deposit*/
+
         double newBalance = account.getAccountBalance()+depositAmount;
         account.setAccountBalance(newBalance);
         System.out.println("Your deposit is successful and your balance is: "+newBalance);
     }
 
     public void checkBalance(Account account){
+        /*check balance*/
+
         double currentBalance = account.getAccountBalance();
         System.out.println("Your account balance is: "+currentBalance);
     }
 
     public void interestRate(Account account){
+        /*get interest*/
+
         double currentInterest = account.getAccountBalance()*BankingApp.getDefaultInterestRate();
         System.out.println("Your account interest is: "+currentInterest);
     }
 
     public void transaction(Account account,Double amount,int receiversAccountNumber){
+        /*Function for Transaction*/
+
         if (account.getAccountBalance() >= amount){
+            /*For check account has sufficient balance*/
+
             Account receiverAccount = getAccountByID(receiversAccountNumber);
+
             if (receiverAccount == null){
+                /*check there is receivers account*/
+
                 System.out.println("Is there no receiver's account");
+
             }else {
                 double newBalance = account.getAccountBalance()-amount;
                 account.setAccountBalance(newBalance);
+
                 double receiversAccountBalance = receiverAccount.getAccountBalance()+amount;
                 receiverAccount.setAccountBalance(receiversAccountBalance);
+
                 System.out.println("Your transaction is successful and your currently account balance is:" +newBalance);
             }
         }else {
             System.out.println("Not sufficient balance for this transaction");
         }
-
 
     }
 }
